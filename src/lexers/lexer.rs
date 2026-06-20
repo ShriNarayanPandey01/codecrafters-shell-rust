a67dc32457170fcd353756d937e0baff9d229b83
ShriNarayanPandey01 <sircaptainharsh@gmail.com>
2026-06-20 19:21:09 +0530

    codecrafters submit [skip ci]

--- src/lexers/lexer.rs
+++ src/lexers/lexer.rs
@@ -4,16 +4,55 @@ pub struct Lexer;
 
 impl Lexer {
     pub fn tokenize(input: &str) -> Vec<Token> {
-        input
-            .split_whitespace()
-            .map(|word| match word {
-                "|" => Token::Pipe,
-                ";" => Token::Semicolon,
-                "&" => Token::Ampersand,
-                "(" => Token::LeftParen,
-                ")" => Token::RightParen,
-                _ => Token::Word(word.to_string()),
-            })
-            .collect()
+        let mut tokens = Vec::new();
+        let mut current_word = String::new();
+        let mut chars = input.chars().peekable();
+        let mut building_word = false;
+
+        while let Some(ch) = chars.next() {
+            match ch {
+                '\'' => {
+                    building_word = true;
+                    for quoted_char in chars.by_ref() {
+                        if quoted_char == '\'' {
+                            break;
+                        }
+
+                        current_word.push(quoted_char);
+                    }
+                }
+                ' ' | '\t' | '\n' | '\r' => {
+                    if building_word {
+                        tokens.push(Token::Word(std::mem::take(&mut current_word)));
+                        building_word = false;
+                    }
+                }
+                '|' | ';' | '&' | '(' | ')' => {
+                    if building_word {
+                        tokens.push(Token::Word(std::mem::take(&mut current_word)));
+                        building_word = false;
+                    }
+
+                    tokens.push(match ch {
+                        '|' => Token::Pipe,
+                        ';' => Token::Semicolon,
+                        '&' => Token::Ampersand,
+                        '(' => Token::LeftParen,
+                        ')' => Token::RightParen,
+                        _ => unreachable!(),
+                    });
+                }
+                _ => {
+                    building_word = true;
+                    current_word.push(ch);
+                }
+            }
+        }
+
+        if building_word {
+            tokens.push(Token::Word(current_word));
+        }
+
+        tokens
     }
 }
